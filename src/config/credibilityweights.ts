f8ef2141413c84602b407ef8a0d9802c846a62ef
RitikaVarshney-hub <varshneyritika15@gmail.com>
2025-07-28 19:24:00 +0530

    made dyamic credibility score

---
+++ src/config/credibilityweights.ts
@@ -0,0 +1,9 @@
+// src/config/credibilityWeights.ts
+export const CredibilityWeights = {
+  baseScore: 50,
+  sensationalPenalty: -10,
+  academicBoost: 20,
+  anonymousPenalty: -15,
+  officialBoost: 15,
+};
+
